6846bea0d95ba5e41f59b2fe3fbea1f2be1dcb14
Your Name <73336938+KD-96@users.noreply.github.com>
2025-07-03 12:56:54 +0530

    updt

--- src/utils/gardenTypeIcons.jsx
+++ src/utils/gardenTypeIcons.jsx
@@ -1,19 +1,20 @@
 // src/utils/gardenTypeIcons.js
 
 import React from 'react';
-import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
-import NatureIcon from '@mui/icons-material/Nature';
-import AccessibleIcon from '@mui/icons-material/Accessible';
-import ParkIcon from '@mui/icons-material/Park';
-import AgricultureIcon from '@mui/icons-material/Agriculture';
 import CenterFocusWeakIcon from '@mui/icons-material/CenterFocusWeak';
 
+import GrassIcon from '@mui/icons-material/Grass';
+import WaterIcon from '@mui/icons-material/Water';
+import EmojiNatureIcon from '@mui/icons-material/EmojiNature';
+import PaletteIcon from '@mui/icons-material/Palette';
+import AccessibleIcon from '@mui/icons-material/Accessible';
+
 const gardenTypeIcons = {
-    vegetable_fruit: <LocalFloristIcon fontSize="small" />,
-    waterFeature: <NatureIcon fontSize="small" />,
+    vegetable_fruit: <GrassIcon fontSize="small" />,
+    waterFeature: <WaterIcon fontSize="small" />,
     wheelchair_stroller: <AccessibleIcon fontSize="small" />,
-    pollinator_native: <ParkIcon fontSize="small" />,
-    art_sculpture: <AgricultureIcon fontSize="small" />,
+    pollinator_native: <EmojiNatureIcon fontSize="small" />,
+    art_sculpture: <PaletteIcon fontSize="small" />,
     default: <CenterFocusWeakIcon fontSize="small" />
 };
 
